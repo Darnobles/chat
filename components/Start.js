208fc0045ad9e6b93af46dca9a88e7822db5238d
Darnobles <126102632+Darnobles@users.noreply.github.com>
2024-01-08 20:44:09 -0500

    add buttons that change the color of the background

--- components/Start.js
+++ components/Start.js
@@ -7,10 +7,26 @@ import {
   Button,
   TextInput,
   ImageBackground,
+  TouchableOpacity,
 } from "react-native";
 
 const Start = ({ navigation }) => {
   const [name, setName] = useState("");
+  const [button1Color, setButton1Color] = useState("#68c481");
+  const [button2Color, setButton2Color] = useState("#ad92de");
+  const [button3Color, setButton3Color] = useState("#7b9fe0");
+
+  const onPressButton1 = () => {
+    setButton1Color("#68c481"); // Change this to the desired color for Button 1
+  };
+
+  const onPressButton2 = () => {
+    setButton2Color("#ad92de"); // Change this to the desired color for Button 2
+  };
+
+  const onPressButton3 = () => {
+    setButton3Color("#7b9fe0"); // Change this to the desired color for Button 3
+  };
 
   return (
     <View style={styles.container}>
@@ -22,7 +38,30 @@ const Start = ({ navigation }) => {
         onChangeText={setName}
         placeholder="Type your username here"
       />
-      <Button title="Go to Chat" onPress={() => navigation.navigate("Chat")} />
+      <TouchableOpacity
+        style={[styles.button1, { backgroundColor: button1Color }]}
+        onPress={onPressButton1}
+      ></TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button2, { backgroundColor: button2Color }]}
+        onPress={onPressButton2}
+      ></TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button3, { backgroundColor: button3Color }]}
+        onPress={onPressButton3}
+      ></TouchableOpacity>
+      <Button
+        title="Go to Chat"
+        onPress={() =>
+          navigation.navigate(
+            "Chat",
+            { name: name },
+            { button1Color: button1Color },
+            { button2Color: button2Color },
+            { button3Color: button3Color }
+          )
+        }
+      />
     </View>
   );
 };
@@ -42,6 +81,25 @@ const styles = StyleSheet.create({
   },
   image: {
     justifyContent: "center",
+  },
+
+  button1: {
+    alignItems: "center",
+    backgroundColor: "#68c481",
+    width: 50 / 2,
+    brorderRadius: 50,
+  },
+  button2: {
+    alignItems: "center",
+    backgroundColor: "##FF0000",
+    width: 50 / 2,
+    brorderRadius: 50,
+  },
+  button3: {
+    alignItems: "center",
+    backgroundColor: "#7b9fe0",
+    width: 50 / 2,
+    brorderRadius: 50,
   },
 });
 
